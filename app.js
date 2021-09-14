5d2b63cfaaac2e8312d6bcd7f8c70e7e2eb2767e
Olvsmith <89484521+Olvsmith@users.noreply.github.com>
2021-09-14 14:06:46 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
-let animals = ["dog", "fish", "cat", "shark", "cat", "dog"];
-
-for(let i=0;i<animals.length; i++) if (animals[i] === "cat") {
-    document.write("I am a cat");
-    document.write("<br/>");
-
-} else if (animals[i] === "dog") {
-    document.write("borf borf");
-    document.write("<br/>");
-
-} else {
-    document.write("I'm an animal");
-    document.write("<br/>");
+// Fisher-Yates algorithm for shuffling an array.
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 }
 
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Scramble";
-    const animals = [
-        {type: "dog"},
-        {type: "fish"},
-        {type: "cat"}
-        {type: "shark"}
-        {type: "cat"}
-        {type: "dog"}
-    ];
+function scramble(shuffleArray = false) {
+  let htmlString = "";
+  let animals = ["dog", "fish", "cat", "shark", "cat", "dog"];
+  let box = document.getElementsByClassName("box")[0];
+
+  //if we pass true then we want to use the
+  // helper function to mutate the array and
+  // shuffle everything up.
+  if (shuffleArray) shuffle(animals);
+
+  for (let i = 0; i < animals.length; i++) {
+    if (animals[i] === "cat") {
+      htmlString += "I am a cat <br/>";
+    } else if (animals[i] === "dog") {
+      htmlString += "borf borf <br/>";
+    } else {
+      htmlString += "I'm an animal <br/>";
+    }
+  }
+
+  box.innerHTML = htmlString;
 }
+
+scramble();
