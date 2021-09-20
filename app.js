e131e48d4675cafbf38a1862c746b2a55ae0fa66
Olvsmith <89484521+Olvsmith@users.noreply.github.com>
2021-09-20 16:36:08 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,32 +1,8 @@
-// Fisher-Yates algorithm for shuffling an array.
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
-
-function scramble(shuffleArray = false) {
-  let htmlString = "";
-  let animals = ["dog", "fish", "cat", "shark", "cat", "dog"];
-  let box = document.getElementsByClassName("box")[0];
-
-  //if we pass true then we want to use the
-  // helper function to mutate the array and
-  // shuffle everything up.
-  if (shuffleArray) shuffle(animals);
-
-  for (let i = 0; i < animals.length; i++) {
-    if (animals[i] === "cat") {
-      htmlString += "I am a cat <br/>";
-    } else if (animals[i] === "dog") {
-      htmlString += "borf borf <br/>";
-    } else {
-      htmlString += "I'm an animal <br/>";
-    }
-  }
-
-  box.innerHTML = htmlString;
-}
-
-scramble();
+var chrs = [
+    {name:"Optimus Prime", class: "leader", afl:"autobot", vehicle:"truck"},
+    {name:"Megatron", class: "leader", afl:"decepticon", vehicle:"tank"},
+    {name:"Bumblebee", class: "scout", afl:"autobot", vehicle:"car"},
+    {name:"Starscream", class: "scout", afl:"decepticon", vehicle:"plane"},
+    {name:"Ironhide", class: "soldier", afl:"autobot", vehicle:"truck"},
+    {name:"Brawl", class: "soldier", afl:"decepticon", vehicle:"tank"},
+];
