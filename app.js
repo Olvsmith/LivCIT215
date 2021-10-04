6fe2184df9406d96d96de820365a9631915b7761
Olvsmith <89484521+Olvsmith@users.noreply.github.com>
2021-10-04 19:48:45 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,70 +1,45 @@
-var chrs = [
-  { name: "Optimus Prime", class: "leader", afl: "autobot", vehicle: "truck" },
-  { name: "Megatron", class: "leader", afl: "decepticon", vehicle: "tank" },
-  { name: "Bumblebee", class: "scout", afl: "autobot", vehicle: "car" },
-  { name: "Starscream", class: "scout", afl: "decepticon", vehicle: "plane" },
-  { name: "Ironhide", class: "soldier", afl: "autobot", vehicle: "truck" },
-  { name: "Brawl", class: "soldier", afl: "decepticon", vehicle: "tank" },
+let boxes = [];
+let array = [
+  { counter: 0, color: "blue" },
+  { counter: 0, color: "red" },
+  { counter: 0, color: "green" },
 ];
 
-// Helper function to pick the color of the text.
-function pickColor(vehicle) {
-  let color = "";
+function Box(button) {
+  const _self = this;
 
-  switch (vehicle) {
-    case "truck":
-      color = "blue";
-      break;
-    case "tank":
-      color = "green";
-      break;
-    case "car":
-      color = "gold";
-      break;
-    case "plane":
-      color = "white";
-      break;
-  }
+  _self.counter = button.counter;
+  _self.color = button.color;
 
-  return color;
-}
+  _self.ele = document.createElement("div");
+  _self.ele.style.backgroundColor = this.color;
+  _self.ele.style.justifyContent = "center";
+  _self.ele.style.cursor = "pointer";
+  _self.ele.style.alignItems = "center";
+  _self.ele.style.borderRadius = "10px";
+  _self.ele.style.color = "white";
+  _self.ele.style.fontSize = "32px";
+  _self.ele.style.fontWeight = "bold";
+  _self.ele.style.textAlign = "center";
+  _self.ele.style.display = "flex";
+  _self.ele.style.margin = "auto 0";
+  _self.ele.style.height = "100px";
+  _self.ele.style.width = "200px";
 
-// Generates the html for the document.
-function generate() {
-  // Create a blank div element
-  let html = document.createElement("div");
+  _self.ele.innerHTML = this.counter;
 
-  html.className = "characters";
-
-  // Iterates over the array
-  chrs.map((obj) => {
-    let image =
-      "https://cdn4.iconfinder.com/data/icons/proglyphs-free/512/Autobot-512.png";
-
-    if (obj.afl === "decepticon")
-      image =
-        "https://cdn4.iconfinder.com/data/icons/proglyphs-free/512/Decepticon-512.png";
-
-    html.insertAdjacentHTML(
-      "beforeend",
-      `
-            <div class="character ${obj.afl}">
-                <img src="${image}" alt="${obj.name}" />
-                <br />
-                <span>Name: ${obj.name}</span>
-                <br />
-                <span>Class: ${obj.class.toUpperCase()}</span>
-                <br />
-                <span style="color: ${pickColor(obj.vehicle)};">Vehicle: ${
-        obj.vehicle
-      }</span>
-                <br />
-            </div>
-        `
-    );
+  _self.ele.addEventListener("click", function () {
+    _self.increaseNumber();
   });
 
-  document.body.appendChild(html);
+  document.body.appendChild(this.ele);
 }
 
-generate();
+Box.prototype.increaseNumber = function () {
+  this.counter = this.counter + 1;
+  this.ele.innerHTML = this.counter;
+};
+
+for (let i = 0; i < array.length; i++) {
+  boxes.push(new Box(array[i]));
+}
