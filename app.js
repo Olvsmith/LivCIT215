5cadb16e77e0738697ca7d0b59d0e50afb1ec659
Olvsmith <89484521+Olvsmith@users.noreply.github.com>
2021-10-12 15:01:09 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,59 +1,37 @@
-var products = [
-  {
-    id: 0,
-    name: "Apples",
-    price: "2.99",
-    image:
-      "https://banner2.kisspng.com/20180310/jyw/kisspng-plum-tomato-apple-fuji-vector-two-apples-5aa49732abbf96.2950483615207360507035.jpg",
-    color: "red",
-  },
-  {
-    id: 1,
-    name: "Bananas",
-    price: "2.50",
-    image:
-      "https://icon2.kisspng.com/20180125/cyq/kisspng-banana-powder-fruit-cavendish-banana-banana-5a6a705e555d06.3110857015169250223497.jpg",
-    color: "gold",
-  },
-  {
-    id: 2,
-    name: "Oranges",
-    price: "3.50",
-    image:
-      "https://icon2.kisspng.com/20180308/wlw/kisspng-south-africa-blood-orange-mandarin-orange-tangelo-south-africa-imports-of-oranges-5aa10d936cc6c7.6226563815205042114456.jpg",
-    color: "orange",
-  },
-];
-
-const popUp = document.body.querySelector(".popUp");
-popUp.addEventListener("click", () => {
-  popUp.className = "popUp hidden";
-});
+var stringers = [3, "Jerry", "Sandy", 4, "Pepper", 15, "Jocastah", 12];
 
 const init = () => {
-  products.map((ob, idx) => {
+  stringers.map((message) => {
     let html,
-      product = document.createElement("div");
-    product.className = `product ${ob.color}`;
+      dynamicMes = "it is normal.",
+      mes = document.createElement("div");
+    mes.className = `message`;
+
+    if (message.length <= 10 && message.length >= 5) {
+      dynamicMes = "the length is below 10 and greater than 5.";
+    } else if (message.length <= 10) {
+      dynamicMes = "the length is below 10.";
+    } else if (message.length === 15) {
+      dynamicMes = "the length is exactly 15.";
+    }
 
     html = `
-        <img src="${ob.image}" alt="${ob.name}" />
         <div>
-            Product: ${ob.name}
-        </div>
-        <div>
-            Price: $${ob.price}
+          The value is ${message} and ${dynamicMes}
         </div>
     `;
 
-    product.innerHTML = html;
+    mes.innerHTML = html;
 
-    product.addEventListener("click", (event) => {
-      popUp.innerHTML = html;
-      popUp.className = "popUp";
+    mes.addEventListener("click", (event) => {
+      if (mes.className.includes("clicked")) {
+        mes.className = "message";
+      } else {
+        mes.className = "message clicked";
+      }
     });
 
-    document.body.appendChild(product);
+    document.body.appendChild(mes);
   });
 };
 
