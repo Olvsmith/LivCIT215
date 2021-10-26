0962f16a9b1ab50cc87fc71472d6e65360b5546d
Olvsmith <89484521+Olvsmith@users.noreply.github.com>
2021-10-25 22:43:52 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
-var stringers = [3, "Jerry", "Sandy", 4, "Pepper", 15, "Jocastah", 12];
-
-const init = () => {
-  stringers.map((message) => {
-    let html,
-      dynamicMes = "it is normal.",
-      mes = document.createElement("div");
-    mes.className = `message`;
-
-    if (message.length <= 10 && message.length >= 5) {
-      dynamicMes = "the length is below 10 and greater than 5.";
-    } else if (message.length <= 10) {
-      dynamicMes = "the length is below 10.";
-    } else if (message.length === 15) {
-      dynamicMes = "the length is exactly 15.";
-    }
-
-    html = `
-        <div>
-          The value is ${message} and ${dynamicMes}
-        </div>
-    `;
-
-    mes.innerHTML = html;
-
-    mes.addEventListener("click", (event) => {
-      if (mes.className.includes("clicked")) {
-        mes.className = "message";
-      } else {
-        mes.className = "message clicked";
-      }
-    });
-
-    document.body.appendChild(mes);
-  });
-};
-
-init();
+new Vue({
+  el: "#destiny-app",
+  data: {
+    cards: [
+      {
+        name: "Rey",
+        rarity: false,
+        setNumber: 100,
+        affl: "hero",
+        color: "blue",
+        health: 10,
+        img: "https://swdestinydb.com/bundles/cards/en/01/01038.jpg",
+      },
+      {
+        name: "Han Solo",
+        rarity: true,
+        setNumber: 36,
+        affl: "hero",
+        color: "yellow",
+        health: 10,
+        img: "http://images-cdn.fantasyflightgames.com/filer_public/3b/15/3b1513ab-1bf2-46f9-93ec-9fb0246e567b/sws17_main.png",
+      },
+      {
+        name: "General Grievous",
+        rarity: true,
+        setNumber: 50,
+        affl: "villain",
+        color: "red",
+        health: 13,
+        img: "https://i.pinimg.com/originals/67/fe/ca/67feca5ed7040713e386d568860701a8.jpg",
+      },
+      {
+        name: "Darth Vader",
+        rarity: false,
+        setNumber: 5,
+        affl: "villain",
+        color: "blue",
+        health: 11,
+        img: "http://images-cdn.fantasyflightgames.com/filer_public/70/2d/702dba51-4b9f-46db-ad74-36216b9fdbe9/swd13_preview2.jpg",
+      },
+    ],
+  },
+});
