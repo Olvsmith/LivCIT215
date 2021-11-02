f1969e9097a38cc6e1b044817c3fd91f60ccfb3c
Olvsmith <89484521+Olvsmith@users.noreply.github.com>
2021-11-01 22:25:12 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,43 +1,56 @@
-new Vue({
-  el: "#destiny-app",
-  data: {
-    cards: [
-      {
-        name: "Rey",
-        rarity: false,
-        setNumber: 100,
-        affl: "hero",
-        color: "blue",
-        health: 10,
-        img: "https://swdestinydb.com/bundles/cards/en/01/01038.jpg",
+const flashcards = {
+  data() {
+    return {
+      cards: [
+        {
+          question: "What year was IUPUI founded?",
+          answer: "1969",
+          category: "history",
+          flipped: false,
+        },
+        {
+          question: "What is 2+2?",
+          answer: "4",
+          category: "math",
+          flipped: false,
+        },
+        {
+          question: "Is mayonnaise an instrument?",
+          answer: "No",
+          category: "science",
+          flipped: false,
+        },
+      ],
+      form: {
+        question: "",
+        answer: "",
+        category: "history",
       },
-      {
-        name: "Han Solo",
-        rarity: true,
-        setNumber: 36,
-        affl: "hero",
-        color: "yellow",
-        health: 10,
-        img: "http://images-cdn.fantasyflightgames.com/filer_public/3b/15/3b1513ab-1bf2-46f9-93ec-9fb0246e567b/sws17_main.png",
-      },
-      {
-        name: "General Grievous",
-        rarity: true,
-        setNumber: 50,
-        affl: "villain",
-        color: "red",
-        health: 13,
-        img: "https://i.pinimg.com/originals/67/fe/ca/67feca5ed7040713e386d568860701a8.jpg",
-      },
-      {
-        name: "Darth Vader",
-        rarity: false,
-        setNumber: 5,
-        affl: "villain",
-        color: "blue",
-        health: 11,
-        img: "http://images-cdn.fantasyflightgames.com/filer_public/70/2d/702dba51-4b9f-46db-ad74-36216b9fdbe9/swd13_preview2.jpg",
-      },
-    ],
+    };
   },
-});
+  methods: {
+    flip(idx) {
+      this.cards[idx].flipped = !this.cards[idx].flipped;
+    },
+    submitForm(e) {
+      e.preventDefault();
+
+      // Push the new card from form.
+      this.cards.push({
+        question: this.form.question,
+        answer: this.form.answer,
+        category: this.form.category,
+        flipped: false,
+      });
+
+      // Reset form after submit
+      this.form = {
+        question: "",
+        answer: "",
+        category: "history",
+      };
+    },
+  },
+};
+
+Vue.createApp(flashcards).mount("#flashcards-app");
